85f339842dd9e76032dd6c05b21136ee641017cd
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-03 12:48:09 +0530

    file structure changed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-import ExpenseItem from "./components/ExpenseItem";
-import "./App.css";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: "store",
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12), location: "mall", },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: "bank",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: "mall",
-    },
-  ];
+import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+
+const App= () => {
+ 
   return (
     <div className="App">
-      <h2>hello</h2>
-      {
-        expenses.map((expense) =>(
-          <ExpenseItem title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          location= {expense.location}/>
-        ))
-      }
+      <h2>Expense Tracker</h2>
+     <Expenses />
     </div>
   );
 }
