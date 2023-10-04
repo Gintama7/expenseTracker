5e0425acb4d79bcd219c1dfb13d572581c7c8924
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-04 12:33:25 +0530

    using state to change price

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ExpenseDetails=(props) => { 
   
+  const [price,setPrice] = useState(props.amount);
+
+  const clickHandler =()=>{
+    setPrice('100');
+  }
+
   return (
    
     <div className='expense-item__description'>
     <h2>{props.title}</h2>
     <h3>{props.location}</h3>
-    <div className="expense-item__price">{props.amount}</div>
+    <div className="expense-item__price">{price}</div>
+    <button onClick={clickHandler}>Change Price</button>
     </div>
     
   )
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 
 const ExpenseItem=(props) =>{
+  const [title,setTitle] = useState(props.title);
   
-  const clickHandler=(e)=>{
-    e.target.parentElement.remove();    
+  const clickHandler=()=>{
+    // e.target.parentElement.remove(); for removing the element
+    setTitle('Updated');
+
    }
 
   return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
-        <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-         <button onClick={clickHandler}>Remove Item</button>
+        <ExpenseDetails amount={props.amount}  location={props.location}   title={title} />
+         {/* <button onClick={clickHandler}>Remove Item</button> */}
+         <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
