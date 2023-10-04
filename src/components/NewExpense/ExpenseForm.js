c3e256ec89fccf4b570a39e0b1f18ffedc15d045
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-04 17:50:57 +0530

    using multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-   const changeHandler = (e)=>{
-        console.log(e.target.value);
+    const [title,setTitle] = useState('');
+    const [amount,setAmount] = useState('');
+    const [date,setDate] = useState('');
+   const titleChangeHandler = (e)=>{
+        setTitle(e.target.value);
+    }
+    const amountChangeHandler = (e)=>{
+        setAmount(e.target.value);
+    }
+    const dateChangeHandler = (e)=>{
+        setDate(e.target.value);
     }
   return (
       <form action="" className='expenseForm'>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label >Title</label>
-                <input type="text" onChange={changeHandler}  />
+                <input type="text" onChange={titleChangeHandler}  />
             </div>
             <div className="new-expense__control">
                 <label >Amount</label>
-                <input type="number"  onChange={changeHandler} />
+                <input type="number"  onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label >Date</label>
-                <input type="date" onChange={changeHandler} />
+                <input type="date" onChange={dateChangeHandler} />
             </div>
         </div>
      
