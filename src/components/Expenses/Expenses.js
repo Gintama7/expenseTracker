b90f53f8eb6237c7e3001856a46a5f58ca27704f
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-05 19:46:59 +0530

    Implementing the filter feature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,12 +11,15 @@ const Expenses=(props)=> {
   const filterChangeHandler = (selectedYear)=>{
     setFilteredYear(selectedYear);
   }
+  const filteredExpenses = (props.items.filter(item=>{
+    return item.date.getFullYear().toString() === filteredYear ;
+  }))
   
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
        {
-        props.items.map((item) =>(
+        filteredExpenses.map((item) =>(
           <ExpenseItem 
           key={item.id}
           title={item.title}
