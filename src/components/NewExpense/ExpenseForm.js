f87b9d515d21b595c773b819ee1de9b894ffe6ef
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-05 10:46:38 +0530

    adding functionality to submit button

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,7 +5,16 @@ const ExpenseForm = () => {
     const [title,setTitle] = useState('');
     const [amount,setAmount] = useState('');
     const [date,setDate] = useState('');
+
+    // const [userInput,setUserInput] = useState({
+    //     title = '',
+    //     amount='',
+    //     date=''
+    // })
    const titleChangeHandler = (e)=>{
+        // setUserInput((prevState) =>{
+        //     return {...prevState, title : e.target.value};
+        // })
         setTitle(e.target.value);
     }
     const amountChangeHandler = (e)=>{
@@ -14,12 +23,24 @@ const ExpenseForm = () => {
     const dateChangeHandler = (e)=>{
         setDate(e.target.value);
     }
+
+    const submitHandler=(e)=>{
+        e.preventDefault();
+        const obj = {title,amount,date};
+        console.log(obj);
+    }
+    // // const inputChangeHandler=(id,value)=>{    // //    Single Function to handle all inputs
+    // //    if(id=== 'title')
+    // {setTitle(value);
+    // }
+    // // }
   return (
       <form action="" className='expenseForm'>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label >Title</label>
                 <input type="text" onChange={titleChangeHandler}  />
+                {/* <input type="text" onChange={(e)=>inputChangeHandler('title',e.target.value)}  />     Making one function to handle all changes*/}
             </div>
             <div className="new-expense__control">
                 <label >Amount</label>
@@ -32,7 +53,7 @@ const ExpenseForm = () => {
         </div>
      
      <div className="new-expense__actions">
-        <button type='submit'>Submit</button>
+        <button type='submit' onClick={submitHandler}>Submit</button>
      </div>
       </form>
    
