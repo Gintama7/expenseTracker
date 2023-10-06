d6e8f82e564e1822c2601208bd98fa0bca350e79
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-06 11:13:30 +0530

    better design pattern with conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 
 const Expenses=(props)=> {
@@ -15,38 +15,27 @@ const Expenses=(props)=> {
     return item.date.getFullYear().toString() === filteredYear ;
   }))
   
-  let expensesContent = <p>No expenses found</p>;
+ 
 
-  if(filteredExpenses.length === 1)
-  {
-    expensesContent = <div>{filteredExpenses.map((item) =>(
-      <ExpenseItem 
-      key={item.id}
-      title={item.title}
-      amount={item.amount}
-      date={item.date}
-      // location= {expense.location}
-      />
-    ))}
-    <p>Only single Expense here. Please add more...</p>
-    </div>
-  }else if(filteredExpenses.length >1)
-  {
-    expensesContent = filteredExpenses.map((item) =>(
-      <ExpenseItem 
-      key={item.id}
-      title={item.title}
-      amount={item.amount}
-      date={item.date}
-      // location= {expense.location}
-      />
-    ))
-  }
+  // if(filteredExpenses.length === 1)
+  // {
+  //   expensesContent = <div>{filteredExpenses.map((item) =>(
+  //     <ExpenseItem 
+  //     key={item.id}
+  //     title={item.title}
+  //     amount={item.amount}
+  //     date={item.date}
+  //     // location= {expense.location}
+  //     />
+  //   ))}
+  //   <p>Only single Expense here. Please add more...</p>
+  //   </div>
+  // }else 
   
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {expensesContent}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   )
 }
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './ExpensesList.css';
+import ExpenseItem from './ExpenseItem';
+
+const ExpensesList = (props) => {
+
+
+    if(props.items.length === 0)
+    {
+        return <h2 className='expenses-list__fallback'>Found no expenses</h2>
+    }
+    
+
+  return (
+    <ul className='expenses-list'>
+      {
+  
+     props.items.map((item) =>(
+      <ExpenseItem 
+      key={item.id}
+      title={item.title}
+      amount={item.amount}
+      date={item.date}
+      // location= {expense.location}
+      />
+    ))
+  }
+    </ul>
+  )
+}
+
+export default ExpensesList
